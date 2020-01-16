b83e80b34d79f3d5303b27781761d56de2839095
Flaminiat <47352051+Flaminiat@users.noreply.github.com>
2020-01-16 17:36:47 +0100

    updated component to solve merging conflicts

--- client/src/components/HomePage/homepage.component.js
+++ client/src/components/HomePage/homepage.component.js
@@ -1,30 +1,24 @@
-import React from "react";
 import Footer from "../footer/footer.component";
-import Map from "../map/map.component";
+import React, { Component } from 'react';
+import HomeContainer from "../pages/Home/Home.js";
+import Leaflet from "leaflet";
+import "leaflet/dist/leaflet.css";
+import PickCoordMap from "./pickCoordMap.js";
 
 
 
-
-const HomePage = ()=>{
-	return (
-		<div>
-
-			<Map />
-			<Footer />
-		</div>
-	);
+export default class HomePage extends Component {
+                
+                 render() {
 
 
-
-
-
-
-
-
-
-};
-
-
-
-
-export default HomePage;
+					 
+                   return (
+                     <div>
+                       <PickCoordMap />
+                       <HomeContainer />
+                       <Footer />
+                     </div>
+                   );
+                 }
+               };
