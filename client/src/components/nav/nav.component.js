6c6e6b84944c88ef7c13db52b1e9c12d7f85b674
FlaminiaT <flaminiatumino@gmail.com>
2020-01-17 15:45:03 +0100

    files from Andrea

--- client/src/components/nav/nav.component.js
+++ client/src/components/nav/nav.component.js
@@ -1,26 +1,51 @@
 import React from "react";
 
 import Logo from "./Logo/cyf-logo.png";
-import {NavLink } from 'react-router-dom'
+import { NavLink } from "react-router-dom";
 
 import "./nav.style.css";
-import Language from "../Language/Language";
+
 
 
 
 
 const NavBar = () => (
   <>
-    <NavLink to="/">
-      <img className="logo" src={Logo} alt="logo" />
-    </NavLink>
-    <NavLink className="nav" to="/ourwork">
-      Our Work
-    </NavLink>
-    <NavLink className="nav" to="/about">
-      About
-    </NavLink>
-    <Language />
+    <nav className="navbar navbar-expand-lg">
+      <NavLink to="/">
+        <img className="logo" src={Logo} alt="logo" />
+      </NavLink>
+      <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
+        <span className="navbar-toggler-icon"></span>
+      </button>
+
+      <div className="collapse navbar-collapse float-right" id="navbarNavDropdown">
+
+      </div>
+      <ul className="navbar-nav my-2 my-lg-0">
+
+        <li className="nav-item ">
+          <NavLink className="nav-link" to="/home">
+            Home
+					</NavLink>
+        </li>
+        <li className="nav-item ">
+          <NavLink className="nav-link" to="/ourwork">
+            Our Work
+					</NavLink>
+        </li>
+        <li className="nav-item ">
+
+          <NavLink className="nav-link" to="/about">
+            About
+					</NavLink>
+
+        </li>
+       
+      </ul>
+    </nav>
+    <hr size="1" width="1400" color="red"></hr>
+
   </>
 );
 
